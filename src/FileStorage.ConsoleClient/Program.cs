9554bfe98f824e604214e6d29947bc25e3a63740
Nikita Mokhnachev <fu.manchouz@gmail.com>
2021-12-28 23:25:47 +0300

    Add code

--- src/FileStorage.ConsoleClient/Program.cs
+++ src/FileStorage.ConsoleClient/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Flurl.Http;
+
+using static System.Console;
+
+var file = File.ReadAllBytes(@"Assets/wallpaper.jpg");
+var json = File.ReadAllBytes(@"Assets/wallpaper.json");
+
+var baseUrl = args.Length > 0 ? 
+    args[0] :
+    "http://localhost:5015";
+
+var responce = await $"{baseUrl}/api/test/upload"
+    .PostJsonAsync(new
+    {
+        Name = "wallpaper.jpg",
+        File = file,
+        Json = json,
+    });
+
+WriteLine(responce.ResponseMessage);
+WriteLine(await responce.GetStringAsync());
