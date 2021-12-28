9bdd97218528256f1bb767e5f0198462c77c0dc1
Nikita Mokhnachev <fu.manchouz@gmail.com>
2021-12-28 22:53:20 +0300

    Inject SQLite storage

--- src/FileStorage.Api/Program.cs
+++ src/FileStorage.Api/Program.cs
@@ -1,6 +1,10 @@
+using FileStorage.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddTransient<IStorage, SqliteStorage>();
 
 var app = builder.Build();
 
