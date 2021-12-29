93859d2d306a2cc39c92da09f7fc41bfc24c4f24
Nikita Mokhnachev <fu.manchouz@gmail.com>
2021-12-29 17:00:52 +0300

    Add test controller

---
+++ src/FileStorage.Api/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using FileStorage.Api.Data.Transfer;
+using FileStorage.Api.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileStorage.Api;
+
+[ApiController] [Route("api/[controller]/[action]")]
+public class TestController : ControllerBase
+{
+    private readonly IStorage _storage;
+
+    public TestController(IStorage storage)
+    {
+        _storage = storage;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UploadResponce>> Upload([FromBody] UploadRequest request)
+    { 
+        var responce = await _storage.UploadAsync(request);
+        return responce;
+    }
+}
